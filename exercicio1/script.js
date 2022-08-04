f44d502acee81fc4acedaa08fc05306a77e9c7dd
ggabrielaoliveira <gabi.oliveiraa@yahoo.com>
2022-08-04 08:56:49 -0300

    exercicio 1 feito

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,7 @@
-const num = //seu numero aqui
-
-if(){
-    console.log("sua mensagem aqui");
-}
+const num = 4;
+const total= num/2;
+if(total%2===0){
+    console.log("seu numero é par");
+}else {
+    console.log("seu numero é impar")
+};
