e6753352f896ab1ba30314c6a9f9e8c9967e2563
ggabrielaoliveira <gabi.oliveiraa@yahoo.com>
2022-08-04 09:42:32 -0300

    exercicio3 feito

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,20 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade= prompt("Escreva sua nacionalidade aqui!\n(com letra minuscula, por favor)");
+console.log("nacionalidade:",nacionalidade);
+
+if(nacionalidade=="brasileira"){
+    console.log("sua nacionalidade foi aceita")
+}else{}
+if(nacionalidade=="argentina"){
+    console.log("sua nacionalidade foi aceita")
+}else{}
+if(nacionalidade=="uruguaia"){
+    console.log("sua nacionalidade foi aceita")
+}else{}
+if(nacionalidade=="chilena"){
+    console.log("sua nacionalidade foi aceita")
+}else{}
+if(nacionalidade=="colombiana"){
+    console.log("sua nacionalidade foi aceita")
+}else{
+    console.log("nacionalidade não encontrada :(")
+}
