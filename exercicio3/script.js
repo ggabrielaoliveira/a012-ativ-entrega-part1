37b8a969ae1e95fd04f699f8fabd2ea658e30871
ggabrielaoliveira <gabi.oliveiraa@yahoo.com>
2022-08-04 19:39:41 -0300

    add info

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,19 +1,19 @@
-let nacionalidade= prompt("Escreva sua nacionalidade aqui!\n(com letra minuscula, por favor)");
+let nacionalidade= prompt("Escreva sua nacionalidade aqui!").toLowerCase();
 console.log("nacionalidade:",nacionalidade);
 
-if(nacionalidade=="brasileira"){
+if(nacionalidade=="brasileira"|| nacionalidade=="brasileiro"){
     console.log("sua nacionalidade foi aceita")
 }else{}
-if(nacionalidade=="argentina"){
+if(nacionalidade=="argentina"|| nacionalidade=="argentino"){
     console.log("sua nacionalidade foi aceita")
 }else{}
-if(nacionalidade=="uruguaia"){
+if(nacionalidade=="uruguaia"|| nacionalidade=="uruguaio"){
     console.log("sua nacionalidade foi aceita")
 }else{}
-if(nacionalidade=="chilena"){
+if(nacionalidade=="chilena"|| nacionalidade=="chileno"){
     console.log("sua nacionalidade foi aceita")
 }else{}
-if(nacionalidade=="colombiana"){
+if(nacionalidade=="colombiana"|| nacionalidade=="colombiano"){
     console.log("sua nacionalidade foi aceita")
 }else{
     console.log("nacionalidade não encontrada :(")
